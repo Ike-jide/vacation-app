ef5f8dea667f452e5c3f752d604b5e77f8c6a356
ike-jide <ikejideofor@gmail.com>
2023-11-14 22:02:55 +0100

    implementing more useEffects

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,18 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import {sortPlacesByDistance} from './loc.js'
 
+
+
+
 function App() {
+  const storedIds =JSON.parse(localStorage.getItem('selectedPlaces')) || []
+const storedPlaces = storedIds.map((id)=>
+AVAILABLE_PLACES.find((place)=>place.id === id) )
+console.log(storedPlaces)
   const modal = useRef();
   const selectedPlace = useRef();
   const [avilablePlaces,setAvilablePlaces]= useState([])
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
   useEffect(()=>{
     navigator.geolocation.getCurrentPosition((position)=>{
@@ -57,6 +64,10 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || []
+    localStorage.setItem('selectedPlaces',
+    JSON.stringify(storedIds.filter((id)=> id !== selectedPlace.current )))
   }
 
   return (
